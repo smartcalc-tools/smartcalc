6721f40fb9cdf1a539f752a52aea6f9f61c44172
smartcalc-tools <lalbahadur53594@gmail.com>
2026-06-07 17:13:40 +0530

    Update analytics.js

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -3,7 +3,29 @@
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  gtag('js', new Date// Google Analytics
+
+const script = document.createElement("script");
+
+script.async = true;
+
+script.src =
+"https://www.googletagmanager.com/gtag/js?id=G-RDR0C88ZNF";
+
+document.head.appendChild(script);
+
+window.dataLayer =
+window.dataLayer || [];
+
+function gtag(){
+  dataLayer.push(arguments);
+}
+
+window.gtag = gtag;
+
+gtag("js", new Date());
+
+gtag("config", "G-RDR0C88ZNF");));
 
   gtag('config', 'G-RDR0C88ZNF');
 </script>
